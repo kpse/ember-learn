e0faba3aa9fed706ef1d7e09046041cc3dc7baa2
Suo Qin <kpsesuo@gmail.com>
2017-09-05 22:05:15 +1000

    add component

---
+++ app/components/x-counter.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  number: 0,
+  actions: {
+    increment() {
+      let number = this.get('number');
+      this.set('number', number + 1);
+    }
+  }
+});
